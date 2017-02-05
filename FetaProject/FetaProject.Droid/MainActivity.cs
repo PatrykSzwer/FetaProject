20d97ee09831bce294e341bc4c494c857ca52a8e
DudEski <dudeostrowski@gmail.com>
2017-02-05 18:53:05 +0100

    Adding Maps Comments

--- FetaProject/FetaProject.Droid/MainActivity.cs
+++ FetaProject/FetaProject.Droid/MainActivity.cs
@@ -16,19 +16,6 @@ namespace FetaProject.Droid
 	{
 		int count = 1;
 
-        public void OnMapReady(GoogleMap googleMap)
-        {
-            MarkerOptions markerOptions = new MarkerOptions();
-            markerOptions.SetPosition(new LatLng(54.7509615, 17.5710568));
-            markerOptions.SetTitle("My position");
-            googleMap.AddMarker(markerOptions);
-
-            //Option
-            googleMap.UiSettings.ZoomControlsEnabled = true;
-            googleMap.UiSettings.CompassEnabled = true;
-            googleMap.MoveCamera(CameraUpdateFactory.ZoomIn());
-        }
-
         protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
@@ -36,20 +23,49 @@ namespace FetaProject.Droid
             // Set our view from the "main" layout resource
             //SetContentView (Resource.Layout.Main);
 
+            //Ustawianie i wyswietlanie layout'u Mapy
+            //trzeba to bedzie podpiac pod przycisk menu
             SetContentView(Resource.Layout.Map);
             MapFragment mapFragment = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
             mapFragment.GetMapAsync(this);
-
+            GoogleMap map = mapFragment.Map;
+            if (map != null) { }
 
             // Get our button from the layout resource,
             // and attach an event to it
-         /*   Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} FETA clicks!", count++);
-			}; */
-		}
-	}
+            /*   Button button = FindViewById<Button> (Resource.Id.myButton);
+
+               button.Click += delegate {
+                   button.Text = string.Format ("{0} FETA clicks!", count++);
+               }; */
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            //Ustawianie widoku mapy
+            LatLng location = new LatLng(54.7509615, 17.5710568);       //te wartosci warto bedzie wyciagac srednia z markerow
+            CameraPosition.Builder builder = CameraPosition.InvokeBuilder();
+            builder.Target(location);
+            builder.Zoom(15);
+            //builder.Bearing(155);
+            builder.Tilt(30);                                       //kat pochylenia 
+            CameraPosition cameraPosition = builder.Build();
+            CameraUpdate cameraUpdate = CameraUpdateFactory.NewCameraPosition(cameraPosition);
+
+            //Ustawianie markera
+            //trzeba przygotować czytnik do robienia listy wrzucajacej wiecej markerow na mapie
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.SetPosition(new LatLng(54.7509615, 17.5710568));
+            markerOptions.SetTitle("My position");
+            googleMap.AddMarker(markerOptions);
+
+            //Opcje mapy i update'y
+            googleMap.UiSettings.ZoomControlsEnabled = true;
+            googleMap.UiSettings.CompassEnabled = true;
+            googleMap.MoveCamera(CameraUpdateFactory.ZoomIn());
+            googleMap.MoveCamera(cameraUpdate);
+        }
+    }
 }
 
 
