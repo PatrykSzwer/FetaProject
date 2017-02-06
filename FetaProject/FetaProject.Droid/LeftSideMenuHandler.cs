91fe5f5a452328cc9d0c6ad0f4b95acef08bb3d8
DudEski <dudeostrowski@gmail.com>
2017-02-06 02:05:08 +0100

    BoczneMenu--niedokonczone

---
+++ FetaProject/FetaProject.Droid/LeftSideMenuHandler.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SupportActionBarDrawerToggle = Android.Support.V7.App.ActionBarDrawerToggle;
+using Android.Support.V7.App;
+using Android.Support.V4.Widget;
+
+namespace FetaProject.Droid
+{
+    class LeftSideMenuHandler : SupportActionBarDrawerToggle
+    {
+        private ActionBarActivity mHostActivity;
+        private int mOpenedResource;
+        private int mClosedResource;
+
+        public LeftSideMenuHandler(ActionBarActivity host, DrawerLayout drawerLayout, int openedResource, int closedResource) : base(host, drawerLayout, openedResource, closedResource)
+        {
+            mHostActivity = host;
+            mOpenedResource = openedResource;
+            mClosedResource = closedResource;
+        }
+
+        public override void OnDrawerOpened(View drawerView)
+        {
+            base.OnDrawerOpened(drawerView);
+            mHostActivity.SupportActionBar.SetTitle(mOpenedResource);
+        }
+
+        public override void OnDrawerClosed(View drawerView)
+        {
+            base.OnDrawerClosed(drawerView);
+            mHostActivity.SupportActionBar.SetTitle(mClosedResource);
+        }
+
+        public override void OnDrawerSlide(View drawerView, float slideOffset)
+        {
+            base.OnDrawerSlide(drawerView, slideOffset);
+        }
+    }
+}
--- FetaProject/FetaProject.Droid/MainActivity.cs
+++ FetaProject/FetaProject.Droid/MainActivity.cs
@@ -8,42 +8,70 @@ using Android.Widget;
 using Android.OS;
 using Android.Gms.Maps;
 using Android.Gms.Maps.Model;
+using SupportTooblar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.V7.App;
+using Android.Support.V4.App;
+using Android.Support.V4.Widget;
 
 namespace FetaProject.Droid
 {
-	[Activity (Label = "FetaProject.Droid", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity, IOnMapReadyCallback
-	{
-		int count = 1;
+	[Activity (Label = "FetaProject.Droid", MainLauncher = true, Icon = "@drawable/icon", Theme="@style/AppTheme")]
+	public class MainActivity : ActionBarActivity, IOnMapReadyCallback
+    {
+        private SupportTooblar mToolbar;
+        private LeftSideMenuHandler mLeftSideMenuHandler;
+        private DrawerLayout mDrawerLayout;
+        private ListView mLeftDrawer;
 
         protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
             // Set our view from the "main" layout resource
-            //SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
 
             //Ustawianie i wyswietlanie layout'u Mapy
             //trzeba to bedzie podpiac pod przycisk menu
-            SetContentView(Resource.Layout.Map);
-            MapFragment mapFragment = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
-            mapFragment.GetMapAsync(this);
-            GoogleMap map = mapFragment.Map;
-            if (map != null) { }
+            /* SetContentView(Resource.Layout.Map);
+             MapFragment mapFragment = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
+             mapFragment.GetMapAsync(this); //jak narazie to odpala mape
+             GoogleMap map = mapFragment.Map;
+             if (map != null) { }
+             */
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            /*   Button button = FindViewById<Button> (Resource.Id.myButton);
+            mToolbar = FindViewById<SupportTooblar>(Resource.Id.toolbar);
+            mDrawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            mLeftDrawer = FindViewById<ListView>(Resource.Id.left_drawer);
+            SetSupportActionBar(mToolbar);
 
-               button.Click += delegate {
-                   button.Text = string.Format ("{0} FETA clicks!", count++);
-               }; */
+            mLeftSideMenuHandler = new LeftSideMenuHandler(
+                this,                         //host activity
+                mDrawerLayout,                //drawer layout
+                Resource.String.openDrawer,   //Opened message
+                Resource.String.closeDrawer   //Closed message
+                );
+
+            //mDrawerLayout.SetDrawerListener(mLeftSideMenuHandler);
+            SupportActionBar.SetHomeButtonEnabled(true);
+            SupportActionBar.SetDisplayShowTitleEnabled(true);
+            mLeftSideMenuHandler.SyncState();
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            mLeftSideMenuHandler.OnOptionsItemSelected(item);
+            return base.OnOptionsItemSelected(item);
+        }
+
+        public override void OnSaveInstanceState(Bundle outState, PersistableBundle outPersistentState)
+        {
+            base.OnSaveInstanceState(outState, outPersistentState);
         }
 
         public void OnMapReady(GoogleMap googleMap)
         {
             //Ustawianie widoku mapy
-            LatLng location = new LatLng(54.7509615, 17.5710568);       //te wartosci warto bedzie wyciagac srednia z markerow
+            LatLng location = new LatLng(54.7540398, 17.5678295);       //te wartosci warto bedzie wyciagac srednia z markerow
             CameraPosition.Builder builder = CameraPosition.InvokeBuilder();
             builder.Target(location);
             builder.Zoom(15);
@@ -55,7 +83,7 @@ namespace FetaProject.Droid
             //Ustawianie markera
             //trzeba przygotować czytnik do robienia listy wrzucajacej wiecej markerow na mapie
             MarkerOptions markerOptions = new MarkerOptions();
-            markerOptions.SetPosition(new LatLng(54.7509615, 17.5710568));
+            markerOptions.SetPosition(new LatLng(54.7540398, 17.5678295));
             markerOptions.SetTitle("My position");
             googleMap.AddMarker(markerOptions);
 
