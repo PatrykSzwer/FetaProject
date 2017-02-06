9a3ffcc69a2ed312fe96bb6e47058ec87af3151b
PatrykSzwer <patryk.szwer@gmail.com>
2017-02-06 23:13:52 +0100

    Added change language functionality.

--- FetaProject/FetaProject.Droid/MainActivity.cs
+++ FetaProject/FetaProject.Droid/MainActivity.cs
@@ -1,35 +1,50 @@
-﻿using System;
-
-using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.OS;
-
-namespace FetaProject.Droid
+﻿namespace FetaProject.Droid
 {
-	[Activity (Label = "FetaProject.Droid", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity
-	{
-		int count = 1;
+    using Android.App;
+    using Android.Content;
+    using Android.OS;
+    using Android.Widget;
 
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
+    using Java.Util;
 
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+    [Activity(Label = "Feta application", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} FETA clicks!", count++);
-			};
-		}
-	}
+            this.SetContentView(Resource.Layout.Main);
+
+            var engButton = this.FindViewById<Button>(Resource.Id.enButton);
+            var polButton = this.FindViewById<Button>(Resource.Id.plButton);
+
+            engButton.Click += (sender, e) => this.ChangeLanguage(Locale.Us);
+            polButton.Click += (sender, e) => this.ChangeLanguage(new Locale("pl_PL", "PL"));
+        }
+
+        /// <summary>
+        /// Changes application internal language.
+        /// </summary>
+        /// <param name="locale">
+        /// The locale to be set.
+        /// </param>
+        private void ChangeLanguage(Locale locale)
+        {
+            this.Resources.Configuration.SetLocale(locale);
+            this.BaseContext.Resources.UpdateConfiguration(this.Resources.Configuration, this.BaseContext.Resources.DisplayMetrics);
+
+            // TODO: Show default application view when implemented.
+            this.DisplayExampleView();
+        }
+
+        // Display example view - to be removed.
+        private void DisplayExampleView()
+        {
+            var intent = new Intent(this, typeof(ExampleActivity));
+            this.StartActivity(intent);
+        }
+    }
 }
 
 
