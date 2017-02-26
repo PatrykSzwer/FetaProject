a94a73c348412bd9dc736f91d3461aa0d4761261
Gagatek5 <tomek.twa@me.com>
2017-02-26 21:00:32 +0100

    Add MapViewControler

---
+++ FetaProject/FetaProject.iOS/MapViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace FetaProject.iOS
+{
+    public partial class MapViewController : UIViewController
+    {
+        public MapViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
