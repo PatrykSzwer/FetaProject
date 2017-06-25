57a7789aa8dc50ad9e0ce508bd5da321dfa49a67
PatrykSzwer <patryk.szwer@gmail.com>
2017-06-25 21:30:34 +0200

    Removed old navigation menu, added new navigation bar based on BottomNavigationView. Big refactor of .cs files.

---
+++ FetaProject.Droid/Helpers/NavigationHelper.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Support.Design.Widget;
+using Android.Widget;
+
+namespace FetaProject.Droid.Helpers
+{
+    internal class NavigationHelper
+    {
+        public static void SetNavigationChange(Activity context)
+        {
+            var bottomNavigation = context.FindViewById<BottomNavigationView>(Resource.Id.bottom_navigation);
+            bottomNavigation.NavigationItemSelected += (s, e) =>
+            {
+                switch (e.Item.ItemId)
+                {
+                    case Resource.Id.bottomNav_Program:
+                        Toast.MakeText(context, "Program clicked", ToastLength.Short).Show();
+                        break;
+                    case Resource.Id.bottomNav_About:
+                        Toast.MakeText(context, "About clicked", ToastLength.Short).Show();
+                        break;
+                    case Resource.Id.bottomNav_Gallery:
+                        Toast.MakeText(context, "Gallery clicked", ToastLength.Short).Show();
+                        break;
+                    case Resource.Id.bottomNav_Map:
+                        Toast.MakeText(context, "Map clicked", ToastLength.Short).Show();
+                        break;
+                    case Resource.Id.bottomNav_Settings:
+                        Toast.MakeText(context, "Settings clicked", ToastLength.Short).Show();
+                        break;
+                }
+            };
+        }
+    }
+}
--- FetaProject.Droid/MainActivity.cs
+++ FetaProject.Droid/MainActivity.cs
@@ -1,87 +1,19 @@
 ﻿using Android.App;
-using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
-using System;
-using BottomNavigationBar;
-using Android.Support.V4.Content;
+using FetaProject.Droid.Helpers;
 
 namespace FetaProject.Droid
 {
-	[Activity(Label = "FetaProject.Droid", MainLauncher = true, Icon = "@mipmap/icon")]
-	public class MainActivity : AppCompatActivity, BottomNavigationBar.Listeners.IOnMenuTabClickListener
-	{
-		private BottomBar _bottomBar;
-		private static string menuColor = "#3a3a3a";
-		private static int menuItemCount = 5;
-
-		protected override void OnCreate(Bundle savedInstanceState)
-		{
-			base.OnCreate(savedInstanceState);
-
-			// Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
-
-			_bottomBar = BottomBar.Attach(this, savedInstanceState);
-			_bottomBar.SetItems(Resource.Menu.bottombar_menu);
-			_bottomBar.SetOnMenuTabClickListener(this);
-
-			// Setting colors for different tabs when there's more than three of them.
-			// You can set colors for tabs in three different ways as shown below.
-
-			for (int i = 0; i < menuItemCount; i++)
-			{
-				_bottomBar.MapColorForTab(i, menuColor);
-			}
-
-            //FindViewById<Button>(Resource.Id.GalleryItem).Click += showGalleryActivity;
-
-		}
-
-		protected override void OnSaveInstanceState(Bundle outState)
-		{
-            base.OnSaveInstanceState(outState);
-			// Necessary to restore the BottomBar's state, otherwise we would
-			// lose the current tab on orientation change.
-			_bottomBar.OnSaveInstanceState(outState);
-			
-		}
-	
-		public void OnMenuTabReSelected(int menuItemId)
-		{
-
-            if (menuItemId == Resource.Id.GalleryItem)
-            {
-                StartActivity(typeof(GalleryActivity));
-
-            }
-            else if (menuItemId == Resource.Id.ProgramItem)
-            {
-                StartActivity(typeof(MainActivity));
-
-            }
-            else if (menuItemId == Resource.Id.AboutItem)
-            {
-                StartActivity(typeof(AboutFestivalActivity));
-
-            }
-            else if (menuItemId == Resource.Id.SettingsItem)
-            {
-                StartActivity(typeof(SettingsActivity));
-
-            }
-            else if (menuItemId == Resource.Id.MapItem)
-            {
-                StartActivity(typeof(MapActivity));
-
-            }
-		}
-
-        public void OnMenuTabSelected(int menuItemId)
+    [Activity(Label = "FetaProject.Droid", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
         {
-			
-        }
+            base.OnCreate(savedInstanceState);
 
+            SetContentView(Resource.Layout.Main);
+            NavigationHelper.SetNavigationChange(this);
+        }
     }
 }
-
